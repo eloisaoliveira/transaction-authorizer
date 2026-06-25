dc5b2b7896f9c83387bf3277009bc051a823e0c2
eloisaoliveira <eloisa.oliveirasantos@gmail.com>
2026-06-25 20:51:46 -0300

    :tada: initial commit

---
+++ src/main/java/com/eloisaoliveira/transaction_authorizer_api/TransactionAuthorizerApiApplication.java
@@ -0,0 +1,13 @@
+package com.eloisaoliveira.transaction_authorizer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAuthorizerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionAuthorizerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eloisaoliveira/transaction_authorizer_api/TransactionAuthorizerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eloisaoliveira.transaction_authorizer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAuthorizerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
